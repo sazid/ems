d669c5106ca73c44d6fa87d4880375f98df62593
Sazid <sazidozon@gmail.com>
2019-05-31 12:16:54 +0600

    Save submissions into db

--- app/api/student/save_submission.php
+++ app/api/student/save_submission.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('../../data/Question.php');
+require_once('../../data/Submission.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     die("Not allowed");
@@ -15,23 +15,25 @@ $data = [
     'message' => '',
 ];
 
-for ($i = 0; $i < count($submissions); ++$i) {
-    $submissions[$i]['user_id'] = $_SESSION['user_id'];
+try {
+    for ($i = 0; $i < count($submissions); ++$i) {
+        $s = $submissions[$i];
+        $s['user_id'] = $_SESSION['user_id'];
+        Submission::insertSubmission(
+            $s['type'],
+            $s['value'],
+            date('Y-m-d H:i:s'),
+            $s['user_id'],
+            $s['question_id'],
+            $s['exam_id']
+        );
+    }
+
+    $data['success'] = true;
+    $data['message'] = 'Submission successful.';
+} catch (PDOException $exc) {
+    $data['success'] = false;
+    $data['message'] = "Failed to save question.$exc";
 }
-
-// try {
-//     if ($id == -1) {
-//         $result = Question::insertQuestion($title, $type, $mcq_options, $course_id);
-//         $data['id'] = $result['last_insert_id'];
-//     } else {
-//         Question::updateQuestion($id, $title, $type, $mcq_options, $course_id);
-//     }
-
-//     $data['success'] = true;
-//     $data['message'] = 'Question saved successfully.';
-// } catch (PDOException $exc) {
-//     $data['success'] = false;
-//     $data['message'] = "Failed to save question.$exc";
-// }
 
 echo json_encode($data);
